b9cc583f91a50fff6972a0cc1ea6ad3e3933ae2e
Brian Northan <bnorthan@gmail.com>
2017-05-12 05:28:17 -0700

    Add ND4J FFT
    This was sadly a failed mission, as it turns out they are currently not
    supporting complex arrays in ND4J, thus none of the FFT wrappers are
    currently functional.  However ND4J is useful for other things, and this
    commit does implement a starting point for II to INDArray converters.

--- src/main/java/net/imagej/ops/experiments/ConvertersUtility.java
+++ src/main/java/net/imagej/ops/experiments/ConvertersUtility.java
@@ -1,8 +1,14 @@
 package net.imagej.ops.experiments;
 
+import net.imagej.ops.slice.SlicesII;
 import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.ComplexType;
+import net.imglib2.view.Views;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 public class ConvertersUtility {
 
@@ -63,5 +69,89 @@ public class ConvertersUtility {
 
 		return data;
 
+	}
+
+	/**
+	 * Convert ND4J Matrix to an II
+	 */
+	static public <T extends ComplexType<T>> void INDArrayToIIFloat2D(final INDArray in,
+			final IterableInterval<T> out) {
+
+		final Cursor<T> cursor = out.cursor();
+
+		// loop through ever row of the matrix
+		for (int r = 0; r < in.rows(); r++) {
+			final float[] data = in.data().asFloat();
+
+			for (int c = 0; c < in.columns(); c++) {
+				cursor.fwd();
+				cursor.get().setReal(data[c]);
+			}
+		}
+	}
+
+	/**
+	 * Convert II to ND4J INDArray
+	 */
+	static public <T extends ComplexType<T>> void IIToINDArrayFloat2D(final IterableInterval<T> in,
+			final INDArray out) {
+
+		final Cursor<T> cursor = in.cursor();
+
+		// loop through ever row of the matrix
+		for (int r = 0; r < in.dimension(0); r++) {
+			final float[] data = new float[(int) in.dimension(1)];
+
+			for (int c = 0; c < in.dimension(1); c++) {
+				cursor.fwd();
+				data[c] = cursor.get().getRealFloat();
+			}
+
+			Nd4j.copy(Nd4j.create(data), out.getRow(r));
+		}
+	}
+
+	/**
+	 * Convert a ND4J Matrix to an RAI
+	 * 
+	 * @param in
+	 * @param out
+	 */
+	static public <T extends ComplexType<T>> void MatrixToRAIRCFloat(final INDArray in,
+			final RandomAccessibleInterval<T> out) {
+
+		// the slicer and cursor are used to loop through each slice of data
+		final SlicesII<T> outSlicer = new SlicesII<T>(out, new int[] { 0, 1 });
+		final Cursor<RandomAccessibleInterval<T>> outCursor = outSlicer.cursor();
+
+		for (int r = 0; r < in.rows(); r++) {
+			outCursor.fwd();
+
+			VecToIIRCFloat(in.getRow(r).data().asFloat(), Views.iterable(outCursor.get()));
+		}
+
+	}
+
+	/**
+	 * Copy an array to an II
+	 * 
+	 * @param in
+	 * @param out
+	 */
+	static public <T extends ComplexType<T>> void VecToIIRCFloat(final float[] in, final IterableInterval<T> out) {
+
+		final Cursor<T> c = out.cursor();
+
+		final long[] pos = new long[c.numDimensions()];
+
+		while (c.hasNext()) {
+			c.fwd();
+
+			c.localize(pos);
+
+			final int index = (int) (pos[0] * out.dimension(1) + pos[1]);
+
+			c.get().setReal(in[index]);
+		}
 	}
 }
---
+++ src/main/java/net/imagej/ops/experiments/fft/ND4JFloatRealForward2D.java
@@ -0,0 +1,60 @@
+package net.imagej.ops.experiments.fft;
+
+import net.imagej.ops.Contingent;
+import net.imagej.ops.Ops;
+import net.imagej.ops.experiments.ConvertersUtility;
+import net.imagej.ops.special.function.AbstractUnaryFunctionOp;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.ComplexType;
+import net.imglib2.type.numeric.complex.ComplexFloatType;
+import net.imglib2.view.Views;
+
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.api.buffer.util.DataTypeUtil;
+import org.nd4j.linalg.api.complex.IComplexNDArray;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.fft.FFT;
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Ops.Filter.IFFT.class, priority = Priority.LOW_PRIORITY)
+public class ND4JFloatRealForward2D<C extends ComplexType<C>>
+		extends AbstractUnaryFunctionOp<RandomAccessibleInterval<C>, Img<ComplexFloatType>>
+		implements Ops.Filter.FFT, Contingent {
+
+	/**
+	 * Compute an 2D forward FFT using jtransform
+	 */
+	@Override
+	public Img<ComplexFloatType> calculate(final RandomAccessibleInterval<C> in) {
+
+		in.dimension(0);
+		in.dimension(1);
+
+		// get data as an INDArray
+		DataTypeUtil.setDTypeForContext(DataBuffer.Type.FLOAT);
+		final INDArray innd = Nd4j.zeros((int) in.dimension(0), (int) in.dimension(1));
+
+		ConvertersUtility.IIToINDArrayFloat2D(Views.iterable(in), innd);
+
+		// perform fft
+		final IComplexNDArray fftresult = FFT.fftn(innd);
+
+		final long[] fftSize = new long[] { fftresult.rows(), fftresult.columns() };
+
+		return ArrayImgs.complexFloats(fftresult.data().asFloat(), fftSize);
+	}
+
+	@Override
+	public boolean conforms() {
+		if (this.in().numDimensions() != 2) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
