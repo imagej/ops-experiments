82e69f6e8fcc098e26ba6046f66166f23b116e77
Brian Northan <bnorthan@gmail.com>
2018-02-09 07:50:59 -0500

    Move ND4J to tests

--- src/main/java/net/imagej/ops/experiments/ConvertersUtility.java
+++ src/main/java/net/imagej/ops/experiments/ConvertersUtility.java
@@ -1,21 +1,18 @@
 package net.imagej.ops.experiments;
 
+import static org.bytedeco.javacpp.cuda.cudaDeviceSynchronize;
 import static org.bytedeco.javacpp.cuda.cudaMalloc;
 import static org.bytedeco.javacpp.cuda.cudaMemcpy;
-import static org.bytedeco.javacpp.cuda.cudaMemcpyHostToDevice;
 import static org.bytedeco.javacpp.cuda.cudaMemcpyDeviceToHost;
-import static org.bytedeco.javacpp.cuda.cudaDeviceSynchronize;
+import static org.bytedeco.javacpp.cuda.cudaMemcpyHostToDevice;
 
-import org.bytedeco.javacpp.FloatPointer;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
-
-import net.imagej.ops.slice.SlicesII;
 import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
-import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.ComplexType;
-import net.imglib2.view.Views;
+
+import org.bytedeco.javacpp.FloatPointer;
+
+import net.imagej.ops.experiments.CudaUtility;
 
 public class ConvertersUtility {
 
@@ -79,67 +76,6 @@ public class ConvertersUtility {
 	}
 
 	/**
-	 * Convert ND4J Matrix to an II
-	 */
-	static public <T extends ComplexType<T>> void INDArrayToIIFloat2D(final INDArray in,
-			final IterableInterval<T> out) {
-
-		final Cursor<T> cursor = out.cursor();
-
-		// loop through ever row of the matrix
-		for (int r = 0; r < in.rows(); r++) {
-			final float[] data = in.data().asFloat();
-
-			for (int c = 0; c < in.columns(); c++) {
-				cursor.fwd();
-				cursor.get().setReal(data[c]);
-			}
-		}
-	}
-
-	/**
-	 * Convert II to ND4J INDArray
-	 */
-	static public <T extends ComplexType<T>> void IIToINDArrayFloat2D(final IterableInterval<T> in,
-			final INDArray out) {
-
-		final Cursor<T> cursor = in.cursor();
-
-		// loop through ever row of the matrix
-		for (int r = 0; r < in.dimension(0); r++) {
-			final float[] data = new float[(int) in.dimension(1)];
-
-			for (int c = 0; c < in.dimension(1); c++) {
-				cursor.fwd();
-				data[c] = cursor.get().getRealFloat();
-			}
-
-			Nd4j.copy(Nd4j.create(data), out.getRow(r));
-		}
-	}
-
-	/**
-	 * Convert a ND4J Matrix to an RAI
-	 * 
-	 * @param in
-	 * @param out
-	 */
-	static public <T extends ComplexType<T>> void MatrixToRAIRCFloat(final INDArray in,
-			final RandomAccessibleInterval<T> out) {
-
-		// the slicer and cursor are used to loop through each slice of data
-		final SlicesII<T> outSlicer = new SlicesII<T>(out, new int[] { 0, 1 });
-		final Cursor<RandomAccessibleInterval<T>> outCursor = outSlicer.cursor();
-
-		for (int r = 0; r < in.rows(); r++) {
-			outCursor.fwd();
-
-			VecToIIRCFloat(in.getRow(r).data().asFloat(), Views.iterable(outCursor.get()));
-		}
-
-	}
-
-	/**
 	 * Copy an array to an II
 	 * 
 	 * @param in
@@ -191,7 +127,7 @@ public class ConvertersUtility {
 		return imgfp;
 
 	}
-	
+
 	/**
 	 * Converts from an II to a FloatPointer
 	 * 
@@ -207,13 +143,13 @@ public class ConvertersUtility {
 
 		final long[] pos = new long[3];
 
-		FloatPointer imgfp = new FloatPointer(ii.dimension(0) * ii.dimension(1)*ii.dimension(2));
+		FloatPointer imgfp = new FloatPointer(ii.dimension(0) * ii.dimension(1) * ii.dimension(2));
 
 		while (c.hasNext()) {
 			c.fwd();
 			c.localize(pos);
 
-			final int index = (int) (pos[0] + pos[1] * xd+pos[2]*xd*yd);
+			final int index = (int) (pos[0] + pos[1] * xd + pos[2] * xd * yd);
 
 			imgfp.put(index, c.get().getRealFloat());
 
@@ -222,7 +158,7 @@ public class ConvertersUtility {
 		return imgfp;
 
 	}
-	
+
 	/**
 	 * Converts from a complex II to a FloatPointer
 	 * 
@@ -235,7 +171,7 @@ public class ConvertersUtility {
 		final int xd = (int) ii.dimension(0);
 		final int yd = (int) ii.dimension(1);
 
-		final FloatPointer imgfp=new FloatPointer(xd*yd*2);
+		final FloatPointer imgfp = new FloatPointer(xd * yd * 2);
 		final long[] pos = new long[2];
 
 		while (c.hasNext()) {
@@ -245,8 +181,7 @@ public class ConvertersUtility {
 			final int index = 2 * (int) (pos[0] + pos[1] * xd);
 
 			imgfp.put(index, c.get().getRealFloat());
-			imgfp.put(index+1, c.get().getImaginaryFloat());
-
+			imgfp.put(index + 1, c.get().getImaginaryFloat());
 
 		}
 
@@ -260,8 +195,7 @@ public class ConvertersUtility {
 	 * @param ii
 	 * @return FloatPointer containing the image data
 	 */
-	static public <T extends ComplexType<T>> FloatPointer floatPointerHostToDevice(final FloatPointer in,
-			int size) {
+	static public <T extends ComplexType<T>> FloatPointer floatPointerHostToDevice(final FloatPointer in, int size) {
 		FloatPointer out = new FloatPointer();
 
 		CudaUtility.checkCudaErrors(cudaMalloc(out, size * Float.BYTES));
@@ -270,7 +204,7 @@ public class ConvertersUtility {
 		return out;
 
 	}
-	
+
 	/**
 	 * Converts from FloatPointer Device to FloatPointer on Host
 	 * 
@@ -280,13 +214,12 @@ public class ConvertersUtility {
 	static public <T extends ComplexType<T>> FloatPointer floatPointerDeviceToHost(final FloatPointer device,
 			int size) {
 		FloatPointer host = new FloatPointer(size);
-        cudaDeviceSynchronize();
-        cudaMemcpy(host, device, size*Float.BYTES, cudaMemcpyDeviceToHost);
+		cudaDeviceSynchronize();
+		cudaMemcpy(host, device, size * Float.BYTES, cudaMemcpyDeviceToHost);
 
 		return host;
 
 	}
-
 
 	static public <T extends ComplexType<T>> FloatPointer ii2DToDeviceFloatPointer(final IterableInterval<T> ii) {
 		FloatPointer hostfp = ii2DToFloatPointer(ii);
