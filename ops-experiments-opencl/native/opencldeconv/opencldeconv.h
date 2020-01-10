c584483251cc5c195f395129a844d2abbba4a43e
Brian Northan <bnorthan@gmail.com>
2020-01-10 18:06:50 -0500

    #elif -> #else

--- ops-experiments-opencl/native/opencldeconv/opencldeconv.h
+++ ops-experiments-opencl/native/opencldeconv/opencldeconv.h
@@ -8,7 +8,7 @@
  __declspec(dllexport)  int fft2d(size_t N1, size_t N2, float *h_image, float * h_out);
  __declspec(dllexport)  int fft2d_long(long N1, long N2, long h_image, long h_out, long l_context, long l_queue);
  __declspec(dllexport)  int fftinv2d(size_t N1, size_t N2, float *h_fft, float * h_out);
-#elif
+#else
 extern "C" {
   void test();
   int conv(size_t N1, size_t N2, size_t N3, float *h_image, float *h_psf, float *h_out);
