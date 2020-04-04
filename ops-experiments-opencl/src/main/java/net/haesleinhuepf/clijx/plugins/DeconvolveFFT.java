3bb06b26bfca247dd64423ea6ade886b03631d03
haesleinhuepf <rhaase@mpi-cbg.de>
2020-04-04 14:42:25 +0200

    bugfix: corrected package name

--- ops-experiments-opencl/src/main/java/net/haesleinhuepf/clijx/plugins/DeconvolveFFT.java
+++ ops-experiments-opencl/src/main/java/net/haesleinhuepf/clijx/plugins/DeconvolveFFT.java
@@ -1,14 +1,14 @@
-package net.haesleinnhuepf.clijx.plugins;
+package net.haesleinhuepf.clijx.plugins;
 
 
 import ij.IJ;
 import ij.ImagePlus;
-import net.haesleinhuepf.clij.CLIJ;
 import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
 import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
 import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
 import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
 import net.haesleinhuepf.clij2.utilities.HasAuthor;
 import net.imagej.ops.experiments.filter.deconvolve.OpenCLFFTUtility;
 import org.scijava.plugin.Plugin;
@@ -19,14 +19,14 @@ public class DeconvolveFFT extends AbstractCLIJ2Plugin implements CLIJMacroPlugi
     @Override
     public boolean executeCL() {
         Object[] args = openCLBufferArgs();
-        boolean result = deconvolveFFT(clij, (ClearCLBuffer)( args[0]), (ClearCLBuffer)(args[1]), (ClearCLBuffer)(args[2]));
+        boolean result = deconvolveFFT(getCLIJ2(), (ClearCLBuffer)( args[0]), (ClearCLBuffer)(args[1]), (ClearCLBuffer)(args[2]));
         releaseBuffers(args);
         return result;
     }
 
-    public static boolean deconvolveFFT(CLIJ clij, ClearCLBuffer input, ClearCLBuffer convolution_kernel, ClearCLBuffer destination) {
+    public static boolean deconvolveFFT(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer convolution_kernel, ClearCLBuffer destination) {
 
-        OpenCLFFTUtility.runDecon(clij, input, convolution_kernel, destination);
+        OpenCLFFTUtility.runDecon(clij2.getCLIJ(), input, convolution_kernel, destination);
 
         return true;
     }
@@ -59,16 +59,16 @@ public class DeconvolveFFT extends AbstractCLIJ2Plugin implements CLIJMacroPlugi
         IJ.run(input, "32-bit", "");
         IJ.run(psf, "32-bit", "");
 
-        CLIJ clij = CLIJ.getInstance("RTX");
+        CLIJ2 clij2 = CLIJ2.getInstance("RTX");
 
-        ClearCLBuffer inputGPU = clij.push(input);
-        ClearCLBuffer psfGPU = clij.push(psf);
+        ClearCLBuffer inputGPU = clij2.push(input);
+        ClearCLBuffer psfGPU = clij2.push(psf);
 
-        ClearCLBuffer output = clij.create(inputGPU);
+        ClearCLBuffer output = clij2.create(inputGPU);
 
-        DeconvolveFFT.deconvolveFFT(clij, inputGPU, psfGPU, output);
+        DeconvolveFFT.deconvolveFFT(clij2, inputGPU, psfGPU, output);
 
-        clij.show(output, "output");
+        clij2.show(output, "output");
 
     }
 }
